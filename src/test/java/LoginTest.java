484bdb594d816b3fd15fcf5e5a199ba60655f835
testgithubfortest <strannyy.strannik@list.ru>
2021-12-02 10:42:56 +0300

    add task 5

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,13 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
+
     public static WebDriver driver;
 
     /**
@@ -15,16 +18,16 @@ public class LoginTest {
      */
     @BeforeAll
     public static void setup() {
-        //определение пути до драйвера и его настройка
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");          //Предпочитаю выносить в proprieties
+        //Запуск драйвера
+        WebDriverManager.chromedriver().setup();
         //создание экземпляра драйвера
-        driver = new ChromeDriver();
-        //окно разворачивается на полный экран
-        driver.manage().window().setSize(new Dimension(500, 500));                      //Также можно использовать proprieties
+        driver = new ChromeDriver(
+                new ChromeOptions().addArguments("--window-size=500, 500")
+        );
         //получение ссылки на страницу входа из файла настроек
         driver.get("https://tt-develop.quality-lab.ru");
         // устанавливаем явное ожидание
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
 
     /**
